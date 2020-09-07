6d31a69ddfae8706ad35cdf434dc2bf4f275f853
Stéphane Emery <stephane.emery@etu.unidistance.ch>
2020-09-07 15:56:18 +0200

    Create a Machine interface and a multi-linear regression
    Creation of a Machine interface from which the algorithm to test will inherit. A first algorithm (Multi-Linear Regression) has been implemented

---
+++ model/MultiLinearRegression.py
@@ -0,0 +1,29 @@
+from Machine import Machine
+from sklearn.linear_model import LinearRegression
+
+class MultiLinearRegression(Machine):
+    def __init__(self):
+        super().__init__('Multi-Linear Regression')
+        self.reg = LinearRegression()
+
+    def train(self, X, y):
+        # Fit data
+        self.reg.fit(X, y)
+
+        self.trained = True
+
+    def test(self, X, y):
+        if not self.trained:
+            print('The algorithm is not trained !')
+            return
+        
+        # Compute the score
+        self.score = self.reg.score(X,y)
+
+    def predict(self, X):
+        if not self.trained:
+            print('The algorithm is not trained !')
+            return
+        
+        return self.reg.predict(X)
+
