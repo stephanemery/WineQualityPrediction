52c1228c4c4b4235e5434ccc8e2acefc9fd969aa
Stéphane Emery <stephane.emery@etu.unidistance.ch>
2020-09-14 11:27:34 +0200

    Add SVM regressor

---
+++ model/SVM.py
@@ -0,0 +1,29 @@
+from .Machine import Machine
+from sklearn.svm import LinearSVR
+
+class SVM(Machine):
+    def __init__(self, epsilon, C):
+        super().__init__('SVM Regressor')
+        self.reg = LinearSVR(epsilon=epsilon, C=C)
+
+    def train(self, X, y):
+        # Fit data
+        self.reg.fit(X, y)
+
+        self.trained = True
+
+    def test(self, X, y):
+        if not self.trained:
+            print('The algorithm is not trained !')
+            return
+        
+        # Compute the score
+        self.score = self.reg.score(X,y)
+
+    def predict(self, X):
+        if not self.trained:
+            print('The algorithm is not trained !')
+            return
+        
+        return self.reg.predict(X)
+
