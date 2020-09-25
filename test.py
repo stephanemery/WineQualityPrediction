b5581d98896f579292e2e4ecd88c3a1fb4f6122a
Augsburger Nassim CHMO <Nassim.Augsburger@syngenta.com>
2020-09-25 16:28:39 +0200

    First use of hypothesis

--- test.py
+++ test.py
@@ -4,12 +4,30 @@ import pandas as pd
 
 from model.Preprocessing import *
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from hypothesis import given, strategies as st
 
+
+@given(x=SortedLists)
 def test_normalize():
-    x = np.random.random((5,3))
+    #x = np.random.random((5,3))
     mean = np.mean(x, axis=0)
     std = np.std(x, axis=0)
     expected = (x-mean)/std 
     result = normalize(pd.DataFrame(x), StandardScaler)
-
     np.testing.assert_array_almost_equal(expected, result, decimal=6)
+    print(test_normalize)
+test_normalize()
+'''
+Values = st.integers()
+SortedLists = st.lists(Values).map(sorted)
+
+def test_normalize():
+
+@given(ls=SortedLists, v=Values)
+def test_insert_is_sorted(ls, v):
+    """We test the first invariant: binary_search should return an index such
+    that inserting the value provided at that index would result in a sorted
+    set."""
+    ls.insert(binary_search(ls, v), v)
+    assert is_sorted(ls)
+'''
