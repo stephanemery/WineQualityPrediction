794f5f4b307bf2749515c5adc1ff221d86698e9b
Augsburger Nassim CHMO <Nassim.Augsburger@syngenta.com>
2020-09-25 16:45:35 +0200

    Change test_normalize  with hypothesis.works

--- test.py
+++ test.py
@@ -5,18 +5,21 @@ import pandas as pd
 from model.Preprocessing import *
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from hypothesis import given, strategies as st
+Values = st.integers()
 
+list1 = st.lists(Values)
+array1=np.array(list1)
 
-@given(x=SortedLists)
-def test_normalize():
+@given(x=array1)
+def test_normalize(x):
+    x = np.array(x)
     #x = np.random.random((5,3))
     mean = np.mean(x, axis=0)
-    std = np.std(x, axis=0)
-    expected = (x-mean)/std 
+    std = np.std  (x, axis=0)
+    expected =    (x-mean)/std 
     result = normalize(pd.DataFrame(x), StandardScaler)
     np.testing.assert_array_almost_equal(expected, result, decimal=6)
-    print(test_normalize)
-test_normalize()
+
 '''
 Values = st.integers()
 SortedLists = st.lists(Values).map(sorted)
