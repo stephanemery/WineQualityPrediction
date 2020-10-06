d9710ebeca108cb452238bed70b5809471c4adfc
Stéphane Emery <stephane.emery@etu.unidistance.ch>
2020-10-06 16:27:22 +0200

    New version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_requirements(f):
 
 setup(
     name="wineQualityPred",
-    version="1.0.3",
+    version="1.0.4",
     description="Wine quality prediction from its physicochimical properties",
     url="https://github.com/stephanemery/WineQualityPrediction",
     license="MIT",
